8f929afb66c0a454e2de8d660b393c3dc5c5092e
Verox001 <altergeist987@gmail.com>
2022-12-14 16:06:35 +0100

    Added authentication system

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import User
 
@@ -6,8 +8,13 @@ from .models import User
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    _user: User = None
 
     def is_valid(self):
-        print(self)
-        users = User.objects.filter(username=self.username)
-        user: User = users.first()
+        self._user = authenticate(username=self.data["username"], password=self.data["password"])
+        return self._user is not None
+
+    def get_user(self):
+        if self._user is None:
+            raise ObjectDoesNotExist("Use is_valid before, trying getting the user.")
+        return self._user
--- manager/models.py
+++ manager/models.py
@@ -2,7 +2,27 @@ from django.contrib.auth.models import Group, AbstractUser
 from django.db import models
 
 # Create your models here.
+YEAR56 = 1
+YEAR7 = 2
+YEAR8 = 3
+YEAR9 = 4
+YEAR10 = 5
+YEAR11 = 6
+YEAR12 = 7
 
+ROLE_CHOICES = (
+    (YEAR56, '5./6. Jahrgang'),
+    (YEAR7, '7. Jahrgang'),
+    (YEAR8, '8. Jahrgang'),
+    (YEAR9, '9. Jahrgang'),
+    (YEAR10, '10. Jahrgang'),
+    (YEAR11, '11. Jahrgang'),
+    (YEAR12, '12. Jahrgang'),
+)
+
+class Years(models.Model):
+    year = models.PositiveBigIntegerField(choices=ROLE_CHOICES, primary_key=True)
+    students = models.IntegerField()
 
 class User(AbstractUser):
     TEACHER = 2
@@ -16,28 +36,17 @@ class User(AbstractUser):
     user_id = models.BigAutoField(primary_key=True)
     email = models.EmailField(max_length=255, unique=True, blank=True, null=True)
     password = models.TextField(max_length=8000)
+    students = models.IntegerField(default=12)
 
     def __str__(self):
         return self.username
 
 
 class Project(models.Model):
-    YEAR56 = 1
-    YEAR7 = 2
-    YEAR8 = 3
-    YEAR9 = 4
-    YEAR10 = 5
-    YEAR11 = 6
-    YEAR12 = 7
-
-    ROLE_CHOICES = (
-        (YEAR56, '5./6. Jahrgang'),
-        (YEAR7, '7. Jahrgang'),
-        (YEAR8, '8. Jahrgang'),
-        (YEAR9, '9. Jahrgang'),
-        (YEAR10, '10. Jahrgang'),
-        (YEAR11, '11. Jahrgang'),
-        (YEAR12, '12. Jahrgang'),
+    DAZS = (
+        (1, "Für Sprachlerner geeignet"),
+        (2, "Nur für fortgeschrittene Sprachlerner geeignet"),
+        (3, "Nicht für Sprachlerner geeignet")
     )
 
     project_id = models.BigAutoField(primary_key=True)
@@ -46,6 +55,7 @@ class Project(models.Model):
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES)
     attendance = models.IntegerField()
     price = models.FloatField(default=0.0)
+    dazs = models.PositiveSmallIntegerField(choices=DAZS, default=1)
     other = models.TextField(max_length=255, blank=True)
 
     def __str__(self):
@@ -55,5 +65,6 @@ class Project(models.Model):
 class ProjectUserConnection(models.Model):
     project_user_connection_id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    partner = models.ForeignKey(User, related_name="partner", on_delete=models.CASCADE, null=True, blank=True)
+    is_partner = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    is_supervisor = models.BooleanField(default=False)
